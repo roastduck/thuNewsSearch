8d7be0e8f76864866aee053c150db45964e1fbab
roastduck <beantang.tang@gmail.com>
2016-09-07 10:46:34 +0800

    query method

--- src/search/models.py
+++ src/search/models.py
@@ -8,6 +8,14 @@ class Pages(models.Model):
     columnName = models.CharField(max_length = 20)
     content = models.TextField()
 
+    @classmethod
+    def satisfyKeys(cls, keys):
+        q = cls.objects
+        for key in keys:
+            inverted = Inverted.objects.filter(key__exact = key).values('pageId')
+            q = q.filter(id__in = inverted)
+        return q
+
 class Inverted(models.Model):
     key = models.CharField(max_length = 10)
     pageId = models.IntegerField()
